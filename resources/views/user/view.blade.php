b8d8bfd41c4f052c25cce14cbe082a02a6def13e
PHetem <p_hetem@hotmail.com>
2023-05-15 20:08:04 -0300

    WIP: Update user views

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.app')
 @section('content')
-<div style="text-align: center; vertical-align: middle; margin-top: 50px;">
-    <div style="margin-top: 30px;">
-        <div style="margin-top: 150px;">
-            <div style="margin: auto; width: 50%;">
-                <span style="font-weight: bold; font-size: 2em;">User Details</span>
-            </div>
-            <div style="width: 50%; margin: auto;">
-                <div style="margin: 25px 40px; width: inherit; height: 300px; border: 1px solid; border-radius:10px; padding: 10px">
+<div style="margin-top: 100px;">
+    <div class="container">
+        <div class="row" style="height: 330px">
+            <span class="cardTitle">User Details</span>
+            <div class="card h-100 w-25" style="overflow: auto">
+                <div class="card-body">
                     <div>
-                        <span><b>ID:</b></span>
+                        <span><b>User ID:</b></span>
                         <span>{{ $user->id }}</span>
                     </div>
                     <div style="margin-top: 15px;">
