5bac1300e1012b7b5d6b7a9e5b0ad50d66c30744
PHetem <p_hetem@hotmail.com>
2023-05-14 21:15:19 -0300

    Add Login/Logout logs

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Listeners\LoginListener;
+use App\Listeners\LogoutListener;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -16,8 +20,14 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
-            SendEmailVerificationNotification::class,
+            SendEmailVerificationNotification::class
         ],
+        Login::class => [
+            LoginListener::class
+        ],
+        Logout::class => [
+            LogoutListener::class
+        ]
     ];
 
     /**
