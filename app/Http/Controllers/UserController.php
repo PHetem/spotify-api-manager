baf6e43801b46b97e6811133cb65b8075bf99dfb
PHetem <p_hetem@hotmail.com>
2023-05-14 21:14:10 -0300

    Add logs to user actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,21 @@ class UserController extends Controller
     public function list() {
         $users = User::all();
 
+        LogController::store('View users');
         return view('user.list', compact('users'));
     }
 
     public function details($id) {
         $user = User::find($id);
 
+        LogController::store('User Viewed: ' . $id);
         return view('user.view', compact('user'));
     }
 
     public function delete($id) {
         User::find($id)->delete();
 
+        LogController::store('User Deleted: ' . $id);
         return redirect()->route('users.list');
     }
 }
