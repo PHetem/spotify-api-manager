d8dde4221a0f14be72aa9cbe45f13561cb64b258
PHetem <p_hetem@hotmail.com>
2023-05-14 21:24:05 -0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Helpers\NavigationHelper;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LogController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\Navigate;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +21,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::middleware([Authenticate::class, Navigate::class])->group(function () {
+
+    Route::controller(NavigationHelper::class)->group(function () {
+        Route::get('/back', 'back')
+            ->withoutMiddleware([Navigate::class])
+            ->name('back');
+    });
+
+    Route::controller(CustomerController::class)->group(function () {
+
+        Route::get('/', 'dashboard')
+            ->name('dashboard');
+
+        Route::get('/customers/details/{id}', 'details')
+            ->name('customers.details');
+
+        Route::get('/customers/delete/{id}', 'delete')
+            ->withoutMiddleware([Navigate::class])
+            ->name('customers.delete');
+    });
+
+    Route::middleware([IsAdmin::class])->group(function () {
+
+        Route::controller(LogController::class)->group(function () {
+            Route::get('/logs/{userID?}', 'list')
+                ->name('logs.list');
+        });
+
+        Route::controller(UserController::class)->group(function () {
+            Route::get('/users', 'list')
+                ->name('users.list');
+
+            Route::get('/users/details/{id}', 'details')
+                ->name('users.details');
+
+            Route::get('/users/delete/{id}', 'delete')
+                ->withoutMiddleware([Navigate::class])
+                ->name('users.delete');
+        });
+    });
 });
