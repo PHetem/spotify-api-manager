077fbe3e3f688f125035515f35e8f13df9e86199
PHetem <p_hetem@hotmail.com>
2023-05-15 19:59:44 -0300

    Add factories to seeders

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -13,54 +13,6 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            [
-                'spotifyID' => Str::random(10),
-                'name' => 'Lucas Medeiros',
-                'email' => 'lucas@mail.com',
-                'country' => 'BR',
-                'profileURL' => 'https://open.spotify.com/user/12167234083',
-                'profilePicture' => fake()->imageUrl(),
-                'followerCount' => rand(1, 999),
-                'accountType' => 'Premium',
-                'accessToken' => Str::random(10),
-                'refreshToken' => Str::random(10)
-            ],[
-                'spotifyID' => Str::random(10),
-                'name' => 'Felipe Medeiros',
-                'email' => 'felipe@mail.com',
-                'country' => 'IE',
-                'followerCount' => rand(1, 999),
-                'profileURL' => 'https://open.spotify.com/user/12167234083',
-                'profilePicture' => fake()->imageUrl(),
-                'accountType' => 'Standard',
-                'accessToken' => Str::random(10),
-                'refreshToken' => Str::random(10)
-            ],[
-                'spotifyID' => Str::random(10),
-                'name' => 'Fernanda souza',
-                'email' => 'fernanda@mail.com',
-                'country' => 'US',
-                'followerCount' => rand(1, 999),
-                'profileURL' => 'https://open.spotify.com/user/12167234083',
-                'profilePicture' => fake()->imageUrl(),
-                'accountType' => 'Premium',
-                'accessToken' => Str::random(10),
-                'refreshToken' => Str::random(10)
-            ],
-        ];
-
-        foreach ($data as $customer) {
-            if (Customer::where('email', '=', $customer['email'])->count()) {
-                $user = Customer::where('email', '=', $customer['email'])->first();
-                $user->update($customer);
-                echo 'User with email ' . $customer['email'] . ' updated' . PHP_EOL;
-            } else {
-                Customer::create($customer);
-                echo 'User with email ' . $customer['email'] . ' created' . PHP_EOL;
-            }
-        }
+        Customer::factory(10)->create();
     }
-    
-
 }
--- database/seeders/LogSeeder.php
+++ database/seeders/LogSeeder.php
@@ -12,22 +12,6 @@ class LogSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = [
-            [
-                'userID' => 1,
-                'action' => 'Login',
-            ],[
-                'userID' => 1,
-                'action' => 'Logout',
-            ],[
-                'userID' => 2,
-                'action' => 'Delete user: Fernanda Cabral',
-            ],
-        ];
-
-        foreach ($data as $log) {
-            Log::create($log);
-            echo 'Log with action ' . $log['action'] . ' created'  . PHP_EOL;
-        }
+        Log::factory(35)->create();
     }
 }
