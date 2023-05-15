5172383933e56dc2454f8d8abaf6d9c5dc4a8b21
PHetem <p_hetem@hotmail.com>
2023-05-14 21:22:33 -0300

    Add logs list view

---
+++ resources/views/logs/view.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+<div style="text-align: center; vertical-align: middle; margin-top: 50px;">
+    <div style="margin-top: 100px;">
+        @if ($logs->isEmpty())
+            <span style="font-weight: bold; font-size: 2em;">No logs avaiable</span>
+        @else
+            <div style="margin-bottom: 25px;">
+                <span style="font-weight: bold; font-size: 2em;">Logs</span>
+            </div>
+            <table class="table table-striped" style="width: 50%; display: inline-table; margin-bottom: 3rem;">
+                <tr>
+                    <td style="width:10%"><b>Id</b></td>
+                    <td style="width:40%"><b>Action</b></td>
+                    <td style="width:30%"><b>Date</b></td>
+                    <td style="width:20%"><b>User</b></td>
+                </tr>
+                <?php foreach ($logs as $log) { ?>
+                    <tr>
+                        <td><b>{{ $log->id }}</b></td>
+                        <td>{{ $log->action }}</td>
+                        <td>{{ $log->created_at }}</td>
+                        <td><a href="{{ route('logs.list', $log->user->id) }}"><b>{{ $log->user->name }}</b><a></td>
+                    </tr>
+              <?php } ?>
+            </table>
+            <div>
+                {{ $logs->links() }}
+            </div>
+        @endif
+    </div>
+</div>
+@endsection
