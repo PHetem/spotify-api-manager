dfb076a11a2b40fc5171e42b2645b3da059067cf
PHetem <p_hetem@hotmail.com>
2023-05-15 22:29:28 -0300

    Add list method to playlistcontroller

--- app/Http/Controllers/PlaylistController.php
+++ app/Http/Controllers/PlaylistController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Playlist;
 
 class PlaylistController extends Controller
 {
-    //
+    public function list($pagination = 30) {
+        $playlists = Playlist::paginate($pagination);
+
+        return $playlists;
+    }
 }
