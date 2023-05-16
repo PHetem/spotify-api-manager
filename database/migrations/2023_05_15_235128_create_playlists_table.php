ec547c199261d57e1f293144a453dfd788393366
PHetem <p_hetem@hotmail.com>
2023-05-15 22:30:41 -0300

    Create playlist table structure

--- database/migrations/2023_05_15_235128_create_playlists_table.php
+++ database/migrations/2023_05_15_235128_create_playlists_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('playlists', function (Blueprint $table) {
             $table->id();
+            $table->string('customerID');
+            $table->string('name');
+            $table->string('coverImageURL');
+            $table->string('URL');
             $table->timestamps();
         });
     }
