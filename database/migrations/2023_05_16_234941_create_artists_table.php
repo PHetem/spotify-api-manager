8c6b60a7436f18a4834d6cef1d79ffe5b657f475
PHetem <p_hetem@hotmail.com>
2023-05-17 14:54:55 -0300

    Add artists migration

--- database/migrations/2023_05_16_234941_create_artists_table.php
+++ database/migrations/2023_05_16_234941_create_artists_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id();
+            $table->string('customerID');
+            $table->string('name');
+            $table->string('profileURL');
+            $table->string('profilePictureURL');
             $table->timestamps();
         });
     }
