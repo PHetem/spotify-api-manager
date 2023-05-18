d870f66a4ee4a0a5c0c383598094d319ee54a214
PHetem <p_hetem@hotmail.com>
2023-05-17 21:44:58 -0300

    Add artist ID to album and tracks table

--- database/migrations/2023_05_16_020848_create_albums_table.php
+++ database/migrations/2023_05_16_020848_create_albums_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->string('customerID');
+            $table->string('artistID');
             $table->string('name');
             $table->string('coverImageURL');
             $table->string('URL');
