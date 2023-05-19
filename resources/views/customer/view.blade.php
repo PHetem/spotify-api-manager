f0d07d37b60c4303b237b6ebcf75f558e555259b
PHetem <p_hetem@hotmail.com>
2023-05-19 19:11:54 -0300

    Add refresh button to customer interface

--- resources/views/customer/view.blade.php
+++ resources/views/customer/view.blade.php
@@ -2,6 +2,16 @@
 @section('content')
 <div style="margin-top: 100px;">
     <div class="container">
+        <div class="row" style="height: 150px">
+            <div class="col-10">
+                <img style="width:-webkit-fill-available; margin-bottom:25px;" class="border rounded" src="{{ asset('img/mock_player.jpg') }}">
+            </div>
+            <div class="col-2">
+                <a href="{{ route('customers.refresh', $customer->id) }}" style="display: block; text-align: center;">
+                    <img style="width:75px; height:75px; margin-top: 50px; margin-bottom: 25px;" src="{{ asset('img/refresh.png') }}">
+                </a>
+            </div>
+        </div>
         <div class="row" style="height: 330px">
             <div class="col-7">
                 @include('customer.details', ['title' => 'Customer Details', 'data' => $customer])
