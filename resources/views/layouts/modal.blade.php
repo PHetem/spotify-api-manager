5969895ada6bcdfce0a02ab94e22ef8e371e0ec7
PHetem <p_hetem@hotmail.com>
2023-06-28 03:31:05 -0300

    Add basic modal template

---
+++ resources/views/layouts/modal.blade.php
@@ -0,0 +1,12 @@
+<div class="modal fade" id="modalBasic" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalTitle">@yield('title')</h5>
+        </div>
+        <div>
+            @yield('modal_content')
+        </div>
+      </div>
+    </div>
+  </div>
