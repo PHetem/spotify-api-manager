ceb218092ffe7ed3dbf8d8215d4aab2bff2054b1
PHetem <p_hetem@hotmail.com>
2023-06-28 03:32:21 -0300

    Add modal to queue view

--- resources/views/customer/player/queue/list.blade.php
+++ resources/views/customer/player/queue/list.blade.php
@@ -1,32 +1,34 @@
-<html>
-    @include('layouts.includes.head')
-    <body>
+@extends('layouts.modal')
+
+@section('title'){{ 'Queue' }}@overwrite
+@section('modal_content')
+
+    @if (empty($queue))
         <div class="table-pos">
-            <div style="margin-top: 100px;">
-                @if (empty($queue))
-                    <span class="main-title">No Tracks found</span>
-                    <p><span class="sub-title">Queued tracks will show up here</span></p>
-                @else
-                    <table class="table table-striped table-half table-fit table-rounded">
-                        @foreach ($queue as $track)
-                            <tr>
-                                <td>
-                                    <div style="display: flex; margin: 0 .5em">
-                                        <div>
-                                            <img src="{{ $track['album']['images'][0]['url'] }}" class="queue-album">
-                                        </div>
-                                        <div>
-                                            <span class="queue-track">{{ $track['name'] }}</span>
-                                            <br>
-                                            <span class="queue-artist">{{ $track['artists'][0]['name'] }}</span>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
-                @endif
+            <div style="margin: 100px;">
+                <span class="main-title">No Tracks found</span>
+                <p><span class="sub-title">Queued tracks will show up here</span></p>
             </div>
         </div>
-    </body>
-</html>
+    @else
+        <table class="table table-striped table-half table-fit table-rounded">
+            @foreach ($queue as $track)
+                <tr>
+                    <td>
+                        <div style="display: flex; margin: 0 .5em">
+                            <div>
+                                <img src="{{ $track['album']['images'][0]['url'] }}" class="queue-album">
+                            </div>
+                            <div>
+                                <span class="queue-track">{{ $track['name'] }}</span>
+                                <br>
+                                <span class="queue-artist">{{ $track['artists'][0]['name'] }}</span>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
+
+@overwrite
