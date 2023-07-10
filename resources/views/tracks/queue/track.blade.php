131aa0da4b3183d0446b795bbdc84431a44574b2
PHetem <p_hetem@hotmail.com>
2023-07-10 06:54:51 -0300

    Update CSS

--- resources/views/tracks/queue/track.blade.php
+++ resources/views/tracks/queue/track.blade.php
@@ -2,12 +2,12 @@
     <td>
         <div style="display: flex; margin: 0 0; align-items: center;" class="row">
             <div class="col-2">
-                <img src="{{ $track->image }}" class="queue-album">
+                <img src="{{ $track->image }}" class="list-album">
             </div>
             <div class="col-10" style="padding-left: 0px;">
-                <span class="queue-track">{{ $track->name }}</span>
+                <span class="list-track">{{ $track->name }}</span>
                 <br>
-                <span class="queue-artist">{{ $track->artist }}</span>
+                <span class="list-artist">{{ $track->artist }}</span>
             </div>
         </div>
     </td>
--- resources/views/tracks/search/track.blade.php
+++ resources/views/tracks/search/track.blade.php
@@ -2,12 +2,12 @@
     <td>
         <div style="display: flex; margin: 0 0; align-items: center;" class="row">
             <div class="col-2">
-                <img src="{{ $track->image }}" class="queue-album">
+                <img src="{{ $track->image }}" class="list-album">
             </div>
             <div class="col-8" style="padding-left: 0px;">
-                <span class="queue-track">{{ $track->name }}</span>
+                <span class="list-track">{{ $track->name }}</span>
                 <br>
-                <span class="queue-artist">{{ $track->artist }}</span>
+                <span class="list-artist">{{ $track->artist }}</span>
             </div>
             <div class="col-2">
                 <button style="float: right;" onclick="switchView('.modal-body', '{{ route('tracks.queue.add', $customerID) }}', {uri: '{{ $track->uri }}', _token: '{{ csrf_token() }}'}, 'POST')" class="btn btn-success add-queue-bt">
