a2bae69030189d72e0a4f031261701514d66c767
PHetem <p_hetem@hotmail.com>
2023-10-01 23:35:13 -0300

    Add no devices available warning

--- resources/views/device/list.blade.php
+++ resources/views/device/list.blade.php
@@ -1,14 +1,24 @@
 @section('title'){{ 'Available Devices' }}@overwrite
 
+
 <div id="content">
-    <table class="table table-striped table-rounded" style="margin-bottom: 0px;">
-        @foreach ($devices as $device)
-            @include('device.device', ['device' => $device])
-        @endforeach
-    </table>
-    <div style="float: right; margin: 15px;">
-        <input type="button" class="btn btn-success" value="Select Device" data-bs-dismiss="modal" route="{{route('customers.details.device.set', $customerID)}}" onclick="selectDevice(this)">
-    </div>
+    @if (empty($devices))
+        <div class="table-pos">
+            <div style="margin: 100px;">
+                <span class="main-title">No Devices Available</span>
+                <p><span class="sub-title">Spotify must be opened on a device for it to become available</span></p>
+            </div>
+        </div>
+    @else
+        <table class="table table-striped table-rounded" style="margin-bottom: 0px;">
+            @foreach ($devices as $device)
+                @include('device.device', ['device' => $device])
+            @endforeach
+        </table>
+        <div style="float: right; margin: 15px;">
+            <input type="button" class="btn btn-success" value="Select Device" data-bs-dismiss="modal" route="{{route('customers.details.device.set', $customerID)}}" onclick="selectDevice(this)">
+        </div>
+    @endif
 </div>
 
 <script>
