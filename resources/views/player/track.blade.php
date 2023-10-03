7814e839286bbafab87a1a6639e45a3a9264bfea
PHetem <p_hetem@hotmail.com>
2023-10-03 15:21:16 -0300

    Add progress bar

--- resources/views/player/track.blade.php
+++ resources/views/player/track.blade.php
@@ -1,5 +1,9 @@
-<div class="border rounded player-track">
-    @if (!is_null($data['track']))
+@php
+    $isPlaying = $data['playingState']->value == 'off';
+@endphp
+<div class="border rounded player-track-border">
+@if (!is_null($data['track']))
+    <div class="player-track">
         <div class="player-button-x-sm" style="align-self: center;">
             <a data-bs-toggle="modal" href="#deviceModal">
                 <img src="{{ asset('img/player/selector-grey.png') }}" class="player-button-x-sm" style="height: 20px; width: 20px;">
@@ -13,6 +17,23 @@
                 <img src="{{ asset('img/player/queue-grey.png') }}" class="player-button-x-sm">
             </a>
         </div>
-    @endif
+    </div>
+    <div id="progressBar" class="player-track-progress" style="width: {{ $data['track']['progressPercentage'] }}%"></div>
+@endif
 </div>
+<script>
+    clearInterval(progressIntervalID)
 
+    var width = {{ $data['track']['progressPercentage'] ?? 100}};
+    var progressIntervalID = setInterval(function () {
+
+        if (!!document.getElementById('progressBar') && {{ json_encode($isPlaying) }}) {
+            if (width >= 100)
+                clearInterval(progressIntervalID)
+
+            width = width + ({{ $data['track']['secondPercentage'] ?? 1}} * 2);
+
+            document.getElementById('progressBar').style.width = width + '%';
+        }
+    }, 2000);
+</script>
